ea6b1ad43de65f73b24e9a7d27cf9d65a9b4b393
Omkar825 <omkarratnala2565@gmail.com>
2025-05-10 00:45:01 +0530

    Update Landing.tsx

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -101,7 +101,7 @@ const Landing: React.FC = () => {
                 onMouseLeave={handleMouseLeave}
               >
                 <img 
-                  src="images/Omkar.png" 
+                  src="/Omkar.png" 
                   alt="Omkara Anjaneya Kumar"
                   className="w-full h-full object-cover"
                   style={{
