f68987f706288149d90fb11f1e75217440aa9fd5
Omkar825 <omkarratnala2565@gmail.com>
2025-05-20 14:41:39 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@emailjs/browser'], // <-- Externalize the package
-    },
-  },
-});
 /// <reference types="vite/client" />
